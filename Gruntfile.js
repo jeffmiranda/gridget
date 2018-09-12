9ce0617680c540c7cd98e9c9015d459975e60050
Jeff Miranda <Jeff.Miranda@mnp.ca>
2018-09-12 11:35:52 -0600

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/**\n' +
+      ' * <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>\n' +
+      ' * <%= pkg.author.url %>\n' +
+      ' **/\n',
+    // Task configuration.
+    sass: {
+      default: {
+        files: [{
+          expand: true,
+          cwd: 'scss',
+          src: ['*.scss'],
+          dest: 'css',
+          ext: '.css'
+        }]
+      }
+    },
+    postcss: {
+      options: {
+        map: {
+          inline: false
+        },
+        processors: [
+          require('autoprefixer')({ browsers: 'last 2 versions' }), // add vendor prefixes
+          require('cssnano')({ "safe": true }) // minify
+        ]
+      },
+      default: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'css',
+          ext: '.min.css'
+        }]
+      }
+    },
+    watch: {
+      // scripts: {
+      //   files: [
+      //     '_build/js/*.js',
+      //     '!_build/js/vendor'
+      //     // '!<%= concat.ltd.dest %>', // don't need to validate the concatenated file because it's already validated
+      //     // '!<%= concat.llp.dest %>' // don't need to validate the concatenated file because it's already validated
+      //   ],
+      //   tasks: ['jshint']
+      // },
+      styles: {
+        files: ['scss/*.scss'],
+        tasks: ['sass', 'postcss']
+      }
+    }
+  });
+
+grunt.event.on('watch', function(action, filepath, target) {
+  grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+});
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-cssnano');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-svgstore');
+  grunt.loadNpmTasks('grunt-postcss');
+
+};
